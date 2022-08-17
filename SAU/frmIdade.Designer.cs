9751164cdaad0706fed1b241b22526c0dd158770
YURI BARBOSA <yuri.barbosa2@senacsp.edu.br>
2022-08-17 19:13:15 -0300

    Formulario Idade: Criação de mensagem de erro para campos não preenchidos e determinação de textbox exclusivo para numeral

--- SAU/frmIdade.Designer.cs
+++ SAU/frmIdade.Designer.cs
@@ -69,10 +69,12 @@ namespace SAU
             // txtIdade
             // 
             this.txtIdade.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.txtIdade.Location = new System.Drawing.Point(120, 130);
+            this.txtIdade.Location = new System.Drawing.Point(120, 127);
             this.txtIdade.Name = "txtIdade";
             this.txtIdade.Size = new System.Drawing.Size(119, 26);
             this.txtIdade.TabIndex = 1;
+            this.txtIdade.TextChanged += new System.EventHandler(this.txtIdade_TextChanged);
+            this.txtIdade.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtIdade_KeyPress);
             // 
             // btnEnviar
             // 
--- SAU/frmIdade.cs
+++ SAU/frmIdade.cs
@@ -23,12 +23,36 @@ namespace SAU
             string nomeUsuario;
             int idadeUsuario;
 
-            // Atribuição de Valores
-            nomeUsuario = txtNome.Text;
-            idadeUsuario = Convert.ToInt32(txtIdade.Text);
+            // Verificar se os campos foram preenchidos
+            if ((txtNome.Text != string.Empty) && (txtIdade.Text != string.Empty))
+            {
 
-            // Apresentação do valor das variáveis
-            MessageBox.Show("Nome: " + nomeUsuario.ToString() + " e idade " + idadeUsuario.ToString());
+                // Atribuição de Valores
+                nomeUsuario = txtNome.Text;
+                idadeUsuario = Convert.ToInt32(txtIdade.Text);
+
+                // Apresentação do valor das variáveis
+                MessageBox.Show("Nome: " + nomeUsuario.ToString() + " e idade " + idadeUsuario.ToString());
+            }
+
+            else
+            {
+                if ((txtNome.Text != string.Empty) && (txtIdade.Text == string.Empty))
+                {
+                    MessageBox.Show("Favor preencher o campo Idade.", "Aviso", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                    txtIdade.Focus();
+                }
+                else if ((txtNome.Text == string.Empty) && (txtIdade.Text != string.Empty))
+                {
+                    MessageBox.Show("Favor preencher o campo Nome.", "Aviso", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                    txtNome.Focus();
+                }
+                else
+                {
+                    MessageBox.Show("Favor preencher os campos.", "Aviso", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                    txtNome.Focus();
+                }
+            }
         }
 
         private void btnLimpar_Click(object sender, EventArgs e)
@@ -38,10 +62,25 @@ namespace SAU
 
             txtNome.Focus();
         }
-        
+
         private void frmIdade_Load(object sender, EventArgs e)
         {
 
+        }
+
+        private void txtIdade_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void txtIdade_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (!char.IsControl(e.KeyChar) && !char.IsDigit(e.KeyChar) &&
+        (e.KeyChar != '.'))
+            {
+                e.Handled = true;
+
+            }
         }
     }
 }
